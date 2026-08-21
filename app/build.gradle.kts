09ef4f02e7c749441d685a48505e60f9e153183d
Addy37 <92006667+Addy37@users.noreply.github.com>
2026-08-21 17:39:59 -0400

    Bump app version to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.addy37.crazyshitunofficial"
         minSdk = 26
         targetSdk = 35
-        versionCode = System.getenv("APP_VERSION_CODE")?.toIntOrNull() ?: 6
-        versionName = System.getenv("APP_VERSION_NAME") ?: "1.4.1"
+        versionCode = System.getenv("APP_VERSION_CODE")?.toIntOrNull() ?: 7
+        versionName = System.getenv("APP_VERSION_NAME") ?: "1.4.2"
     }
 
     val releaseKeystorePath = System.getenv("ANDROID_KEYSTORE_PATH")
