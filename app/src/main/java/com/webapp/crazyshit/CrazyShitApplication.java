3f430dfbb63a86acd9eb0fa22cb2d2160aab5571
Addy37 <92006667+Addy37@users.noreply.github.com>
2026-08-23 12:29:37 -0400

    Hook v2.2 flashy UI lifecycle

--- app/src/main/java/com/webapp/crazyshit/CrazyShitApplication.java
+++ app/src/main/java/com/webapp/crazyshit/CrazyShitApplication.java
@@ -34,6 +34,7 @@ public final class CrazyShitApplication extends Application {
                     LandscapeRailPolish.applySoon(nativeActivity);
                     LandscapeMoreDialog.attachSoon(nativeActivity);
                     UiPolishController.attach(nativeActivity);
+                    FlashUiController.attach(nativeActivity);
                     ChaosPortraitPolish.start(nativeActivity);
                 }
                 if (activity instanceof VideoDetailActivity) {
@@ -61,6 +62,7 @@ public final class CrazyShitApplication extends Application {
                 if (activity instanceof NativeMainActivity) {
                     NativeMainActivity nativeActivity = (NativeMainActivity) activity;
                     ChaosPortraitPolish.stop(nativeActivity);
+                    FlashUiController.detach(nativeActivity);
                     UiPolishController.detach(nativeActivity);
                     LandscapeUiController.detach(nativeActivity);
                     NativeMainActivity current = currentNativeActivity.get();
@@ -81,6 +83,7 @@ public final class CrazyShitApplication extends Application {
                     LandscapeRailPolish.applySoon(activity);
                     LandscapeMoreDialog.attachSoon(activity);
                     UiPolishController.attach(activity);
+                    FlashUiController.attach(activity);
                     ChaosPortraitPolish.start(activity);
                 },
                 80L
