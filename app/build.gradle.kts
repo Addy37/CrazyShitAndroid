f0d29cdf0b53b5230a5fe52a492ae11ffd5f2a96
Addy37 <92006667+Addy37@users.noreply.github.com>
2026-08-25 06:21:31 -0400

    Add AndroidX WebKit for document-start hooks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.browser:browser:1.8.0")
     implementation("androidx.core:core:1.15.0")
+    implementation("androidx.webkit:webkit:1.17.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.viewpager2:viewpager2:1.1.0")
